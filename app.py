e7ffe1ef82bf5c44b5087854a93bd57e6bbcedda
Jossue <josdesi@gmail.com>
2020-05-10 19:45:34 -0500

    Print latex
    LaTeX parsing requires the antlr4 python package,
    antlr4-python3-runtime

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS, cross_origin
 import sympy
 from sympy import *
+from sympy.parsing.latex import parse_latex
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
@@ -9,8 +10,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 
-
-@app.route('/api/v1/info', methods=['GET','OPTIONS'])
+@app.route('/api/v1/info', methods=['GET'])
 @cross_origin(origin='*',headers=['Content-Type','Authorization'])
 def info():
     x,y = symbols('x,y')
@@ -20,6 +20,17 @@ def info():
         "sympy_version": sympy.__version__,
         "polynomial": polynomial,
         "derivative": derivative
+        }
+
+@app.route('/api/v1/polynomial/properties/<polynomial>', methods=['GET'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+def properties(polynomial):
+    x,y = symbols('x,y')    
+    derivative = diff(polynomial, x, 1)
+    return {
+        "polinomio": {
+            "latex":latex(derivative)
+            }        
         }
 
 if __name__ == '__main__':
