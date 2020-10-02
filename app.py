947b76ea8ce8f773c18302685c93582845ff653d
husax17 <hmejia@cinvestav.mx>
2020-10-01 20:03:01 -0500

    cambio para aceptar funciones racionales

--- app.py
+++ app.py
@@ -9,91 +9,92 @@ cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
-
-@app.route('/api/v1/info', methods=['GET'])
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+@app.route('/api/v1/info', methods=['GET', 'POST'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def info():
-    x,y = symbols('x,y')
-    polynomial = '4*x**2'    
+    x, y = symbols('x,y')
+    polynomial = '4*x**2'
     derivative = format(diff(polynomial, x, 1))
     return {
         "sympy_version": sympy.__version__,
         "polynomial": polynomial,
         "derivative": derivative
-        }
+    }
 
-@app.route('/api/v1/polynomial/properties/<poly>', methods=['GET'])
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+
+@app.route('/api/v1/polynomial/properties/<path:poly>', methods=['GET'])
+@cross_origin(origin='http://localhost:3000', headers=['Content-Type', 'Authorization'])
 def properties(poly):
-    x,y = symbols('x,y')
-    polynomial = eval(poly)  
-    derivative = diff(polynomial, x, 1)
+    x, y = symbols('x, y')
+    polynomial = eval(poly)
+    # derivative = diff(polynomial, x, 1)
     derivada = diff(polynomial, x, 1)
-    derivada2 = diff(diff(polynomial, x),x)
-    raices = solve(Eq(polynomial,0),x)
+    derivada2 = diff(derivada, x)
+    raices = solve(Eq(polynomial, 0), x)
     puntosInflexion = []
     return {
         "polinomio": {
-            "expr":format(polynomial),
-            "latex":latex(polynomial)
-            },
-        "derivada":{
-            "expr":format(derivada),
-            "latex":latex(derivada)
-            },
-        "derivada2":{
-            "expr":format(derivada2),
-            "latex":latex(derivada2)
-            },
-        "raices":{
-            "expr":format(raices),
-            "latex":latex(raices)
-            },
-        "puntosInflexion":{
-            "expr":format(puntosInflexion),
-            "latex":latex(puntosInflexion)
-            },
-        "clasifPuntosCriticos":{
-            "expr":format(puntosInflexion),
-            "latex":latex(puntosInflexion)
-            },
-        "puntosCriticos":{
-            "expr":format(puntosInflexion),
-            "latex":latex(puntosInflexion)
-            },
-        "signoFuncPos":{
-            "expr":format(puntosInflexion),
-            "latex":latex(puntosInflexion)
-            },
-        "signoFuncNeg":{
-            "expr":format(puntosInflexion),
-            "latex":latex(puntosInflexion)
-            },
-        "monotoniaPos":{
-            "expr":format(puntosInflexion),
-            "latex":latex(puntosInflexion)
-            },
-        "monotoniaNeg":{
-            "expr":format(puntosInflexion),
-            "latex":latex(puntosInflexion)
-            },
-        "concavidadPos":{
-            "expr":format(puntosInflexion),
-            "latex":latex(puntosInflexion)
-            },
-        "concavidadNeg":{
-            "expr":format(puntosInflexion),
-            "latex":latex(puntosInflexion)
-            },
-        "extensionPos":{
-            "expr":format(puntosInflexion),
-            "latex":latex(puntosInflexion)
-            },
-        "extensionNeg":{
-            "expr":format(puntosInflexion),
-            "latex":latex(puntosInflexion)
-            }
+            "expr": format(polynomial),
+            "latex": latex(polynomial)
+        },
+        "derivada": {
+            "expr": format(derivada),
+            "latex": latex(derivada)
+        },
+        "derivada2": {
+            "expr": format(derivada2),
+            "latex": latex(derivada2)
+        },
+        "raices": {
+            "expr": format(raices),
+            "latex": latex(raices)
+        },
+        "puntosInflexion": {
+            "expr": format(puntosInflexion),
+            "latex": latex(puntosInflexion)
+        },
+        "clasifPuntosCriticos": {
+            "expr": format(puntosInflexion),
+            "latex": latex(puntosInflexion)
+        },
+        "puntosCriticos": {
+            "expr": format(puntosInflexion),
+            "latex": latex(puntosInflexion)
+        },
+        "signoFuncPos": {
+            "expr": format(puntosInflexion),
+            "latex": latex(puntosInflexion)
+        },
+        "signoFuncNeg": {
+            "expr": format(puntosInflexion),
+            "latex": latex(puntosInflexion)
+        },
+        "monotoniaPos": {
+            "expr": format(puntosInflexion),
+            "latex": latex(puntosInflexion)
+        },
+        "monotoniaNeg": {
+            "expr": format(puntosInflexion),
+            "latex": latex(puntosInflexion)
+        },
+        "concavidadPos": {
+            "expr": format(puntosInflexion),
+            "latex": latex(puntosInflexion)
+        },
+        "concavidadNeg": {
+            "expr": format(puntosInflexion),
+            "latex": latex(puntosInflexion)
+        },
+        "extensionPos": {
+            "expr": format(puntosInflexion),
+            "latex": latex(puntosInflexion)
+        },
+        "extensionNeg": {
+            "expr": format(puntosInflexion),
+            "latex": latex(puntosInflexion)
         }
+    }
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
