04af031aa1a7d68a7aa484838cb068cac1659acb
HugoMejia <hmejia@cinvestav.mx>
2021-02-24 14:37:20 -0600

    "calcula simbolicamente func rac separadas en Polin"

--- app.py
+++ app.py
@@ -25,75 +25,84 @@ def info():
 @app.route('/api/v1/polynomial/properties/<path:poly>', methods=['GET'])
 @cross_origin(origin='http://localhost:3000', headers=['Content-Type', 'Authorization'])
 def properties(poly):
-    x, y = symbols('x, y')
-    polynomial = eval(poly)
-    # derivative = diff(polynomial, x, 1)
-    derivada = diff(polynomial, x, 1)
-    derivada2 = diff(derivada, x)
-    raices = solve(Eq(polynomial, 0), x)
-    puntosInflexion = []
-    return {
-        "polinomio": {
-            "expr": format(polynomial),
-            "latex": latex(polynomial)
-        },
-        "derivada": {
-            "expr": format(derivada),
-            "latex": latex(derivada)
-        },
-        "derivada2": {
-            "expr": format(derivada2),
-            "latex": latex(derivada2)
-        },
-        "raices": {
-            "expr": format(raices),
-            "latex": latex(raices)
-        },
-        "puntosInflexion": {
-            "expr": format(puntosInflexion),
-            "latex": latex(puntosInflexion)
-        },
-        "clasifPuntosCriticos": {
-            "expr": format(puntosInflexion),
-            "latex": latex(puntosInflexion)
-        },
-        "puntosCriticos": {
-            "expr": format(puntosInflexion),
-            "latex": latex(puntosInflexion)
-        },
-        "signoFuncPos": {
-            "expr": format(puntosInflexion),
-            "latex": latex(puntosInflexion)
-        },
-        "signoFuncNeg": {
-            "expr": format(puntosInflexion),
-            "latex": latex(puntosInflexion)
-        },
-        "monotoniaPos": {
-            "expr": format(puntosInflexion),
-            "latex": latex(puntosInflexion)
-        },
-        "monotoniaNeg": {
-            "expr": format(puntosInflexion),
-            "latex": latex(puntosInflexion)
-        },
-        "concavidadPos": {
-            "expr": format(puntosInflexion),
-            "latex": latex(puntosInflexion)
-        },
-        "concavidadNeg": {
-            "expr": format(puntosInflexion),
-            "latex": latex(puntosInflexion)
-        },
-        "extensionPos": {
-            "expr": format(puntosInflexion),
-            "latex": latex(puntosInflexion)
-        },
-        "extensionNeg": {
-            "expr": format(puntosInflexion),
-            "latex": latex(puntosInflexion)
+    x = symbols('x')
+    polinL = poly.split(',')
+    if len(polinL) == 1:
+        pol = Poly(polinL[0])
+        deriv1 = diff(pol, x)
+        deriv2 = diff(deriv1, x)
+        raices = aprox(real_roots(pol))
+        raicesD1 = aprox(real_roots(deriv1))
+        raicesD2 = aprox(real_roots(deriv2))
+        return {
+            "polinomio": {
+                "expr": format(pol),
+                "latex": latex(pol),
+            },
+            "derivada": {
+                "expr": format(deriv1),
+                "latex": latex(deriv1),
+            },
+            "derivada2": {
+                "expr": format(deriv2),
+                "latex": latex(deriv2),
+            },
+            "raices": {
+                "rpol": format(raices),
+                "rder1": format(raicesD1),
+                "rder2": format(raicesD2),
+            },
         }
-    }
+    elif len(polinL) == 2:
+        polNum, polDen = polinL
+        polNum = Poly(polNum)
+        polDen = Poly(polDen)
+        raices = aprox(real_roots(polNum))
+        polos = aprox(real_roots(polDen))
+        rac = polNum/polDen
+        derNum = diff(polNum, x)*polDen-polNum*diff(polDen, x)
+        deriv1 = diff(rac, x)
+        deriv2 = diff(deriv1, x)
+        der2Num = diff(derNum, x)*polDen**2 - 2 * \
+            polDen*diff(polDen, x)*derNum
+        raicesD1 = aprox(real_roots(derNum))
+        raicesD2 = aprox(real_roots(der2Num))
+        return {
+            "polNum": {
+                "expr": format(polNum),
+                "latex": latex(polNum)
+            },
+            "polDen": {
+                "expr": format(polDen),
+                "latex": latex(polDen)
+            },
+            "derivada": {
+                "expr": format(deriv1),
+                "latex": latex(deriv1)
+            },
+            "derivada2": {
+                "expr": format(deriv2),
+                "latex": latex(deriv2)
+            },
+            "raices": {
+                "polNum": format(raices),
+                "derNum": format(raicesD1),
+                "der2Num": format(raicesD2)
+            },
+            "polos": format(polos),
+        }
+    else:
+        return {"algo": "algo salio mal"}
+
+
+def aprox(raices):
+    rAprox = []
+    for r in raices:
+        if isinstance(r, Rational):
+            rAprox.append(r)
+        else:
+            rAprox.append(r.evalf(15))
+    return rAprox
 
 
 if __name__ == '__main__':
